25a236ef90353db09b60fe4ed26a41adcb7d15b8
PhilippePego95 <p.gonzalezmiralles@gmail.com>
2016-09-29 10:09:35 +0200

    solucion del profesor

--- solucion/solucion/Program.cs
+++ solucion/solucion/Program.cs
@@ -39,37 +39,66 @@ namespace PCategoria
 
 			}
 		}
-
-		private static string INSERT_SQL = "insert into categoria (nombre) values (@nombre)";
 		private static string UPDATE_SQL = "update categoria set nombre=@nombre where id=@id";
+		private static string INSERT_SQL = "insert into categoria (nombre) values (@nombre)";
 		private static string DELETE_SQL = "delete from categoria where id=@id";
+		private static string SELECT_SQL = "select * from categoria order by id";
 
 		private static void nuevo() {
 			string nombre = readString("Nombre: ");
 			IDbCommand dbCommand = dbConnection.CreateCommand ();
 			dbCommand.CommandText = INSERT_SQL;
 			addParameter (dbCommand, "nombre", nombre);
-			dbCommand.ExecuteNonQuery ();
+			try {
+				dbCommand.ExecuteNonQuery ();
+			} catch (MySqlException ex) {
+				Console.WriteLine (getUserMessage(ex));				
+			}
 		}
 
 
 		private static void editar() {
 			long id = readLong ("Id: ");
 			string nombre = readString("Nombre: ");
-
+			IDbCommand dbCommand = dbConnection.CreateCommand ();
+			dbCommand.CommandText = UPDATE_SQL;
+			addParameter (dbCommand, "id", id);
+			addParameter (dbCommand, "nombre", nombre);
+			try {
+				int filas = dbCommand.ExecuteNonQuery (); //devolverá 0 o 1
+				if (filas == 0)
+					Console.WriteLine ("Id no existente. No existe ningún registro con ese Id.");
+			} catch (MySqlException ex) {
+				Console.WriteLine (getUserMessage(ex));				
+			}
 		}
-
-
 		private static void borrar() {
 			long id = readLong ("Id: ");
 			IDbCommand dbCommand = dbConnection.CreateCommand ();
 			dbCommand.CommandText = DELETE_SQL;
 			addParameter (dbCommand, "id", id);
-			//TODO comprobar si devuelve 0 (ningún registro afectado)...
-			dbCommand.ExecuteNonQuery ();
+			int filas = dbCommand.ExecuteNonQuery (); //devolverá 0 o 1
+			if (filas == 0)
+				Console.WriteLine ("Id no existente. No existe ningún registro con ese Id.");
 		}
 
 		private static void listar() {
+			IDbCommand dbCommand = dbConnection.CreateCommand ();
+			dbCommand.CommandText = SELECT_SQL;
+			IDataReader dataReader = dbCommand.ExecuteReader ();
+			Console.WriteLine("{0,5} {1}", "Id", "Nombre");
+			while (dataReader.Read()) 
+				Console.WriteLine ("{0,5} {1}", dataReader ["id"], dataReader ["nombre"]); 
+			dataReader.Close ();
+		}
+
+		private const int ER_DUP_ENTRY = 1062;
+		private static string getUserMessage(MySqlException ex) {
+			switch (ex.Number) {
+				case ER_DUP_ENTRY:
+				return "Dato duplicado. Ese dato ya existe.";
+			}
+			return ex.Message;
 		}
 
 		private static void addParameter(IDbCommand dbCommand, string name, object value) {
