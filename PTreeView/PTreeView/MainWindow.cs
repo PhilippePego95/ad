15884deeb12a699d3c035052c8b865f91e525010
PhilippePego95 <p.gonzalezmiralles@gmail.com>
2016-09-29 11:27:41 +0200

    treeview con la opcion de listar articulo

--- PTreeView/PTreeView/MainWindow.cs
+++ PTreeView/PTreeView/MainWindow.cs
@@ -1,22 +1,39 @@
 using System;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 using Gtk;
 
-public partial class MainWindow: Gtk.Window
-{	
-	public MainWindow (): base (Gtk.WindowType.Toplevel)
-	{
+public partial class MainWindow: Gtk.Window {
+	public static IDbConnection dbConnection;
+	private static string SELECT_SQL = "select * from articulo order by id";
+
+	public MainWindow (): base (Gtk.WindowType.Toplevel) {
 		Build ();
+		dbConnection = new MySqlConnection ("Database=dbprueba;User Id=root;Password=sistemas");
+		dbConnection.Open ();
+
 		treeView.AppendColumn ("id", new CellRendererText (), "text", 0);
 		treeView.AppendColumn ("nombre", new CellRendererText (), "text", 1);
-		ListStore listStore = new ListStore (typeof(long), typeof(string));
-		treeView.Model = listStore;
-		listStore.AppendValues (1L, "categoria 1");
+		treeView.AppendColumn ("precio", new CellRendererText (), "text", 2);
+		treeView.AppendColumn ("categoria", new CellRendererText (), "text", 3);
 
+		ListStore listStore = new ListStore (typeof(long), typeof(string), typeof(string), typeof(long));
+
+		treeView.Model = listStore;
+		IDbCommand dbCommand = dbConnection.CreateCommand ();
+		dbCommand.CommandText = SELECT_SQL;
+		IDataReader dataReader = dbCommand.ExecuteReader ();
+		while (dataReader.Read()) 
+		listStore.AppendValues (dataReader["id"], dataReader ["nombre"], ""+dataReader["precio"],dataReader["categoria"]);
+		dataReader.Close ();
 	}
 
-	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
-	{
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a) {
 		Application.Quit ();
 		a.RetVal = true;
+	}
+	public void listar(){
+
 	}
 }
