ce66662fb8cf13d6d41acc50c99d6acd32eb3d43
PhilippePego95 <p.gonzalezmiralles@gmail.com>
2016-10-04 11:51:40 +0200

    PTreeVieew con un delegado y añadiendo un metodo en una libreria

--- PTreeView/Org.InstitutoSerpis.Ad/TreeViewHelper.cs
+++ PTreeView/Org.InstitutoSerpis.Ad/TreeViewHelper.cs
@@ -1,15 +1,13 @@
 using System;
 using Gtk;
-namespace Org.Institutoserpis.Ad
+namespace Org.InstitutoSerpis.Ad
 {
 	public class TreeViewHelper	{
 
 		public static void AppendColumns(TreeView treeView, string [] columnsNames){
-			int index = 0;
 			foreach (string columName in columnsNames) {
-			
 				treeView.AppendColumn (columName, new CellRendererText (),
-				    delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter tree_iter){
+				    delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter){
 						int Column = Array.IndexOf(treeView.Columns, tree_column);
 						CellRendererText cellRendererText = (CellRendererText)cell;
 						object value = tree_model.GetValue(iter, 2);
