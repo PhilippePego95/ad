824d7c435ace27f7ac8bd25dbcf1321c5f12f47f
PhilippePego95 <p.gonzalezmiralles@gmail.com>
2016-11-29 10:28:43 +0100

    em creat el primer hola món amb gradle,GHola Món en Eclipse

---
+++ GHolaMon/src/main/java/org/institutoserpis/ad/HolaMon.java
@@ -0,0 +1,11 @@
+package org.institutoserpis.ad;
+
+public class HolaMon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola món desde eclipse amb Gradle");
+
+	}
+
+}
